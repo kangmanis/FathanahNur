43a0da7720f201c950ed1a31382e93572dffc585
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2022-12-10 10:54:31 +0800

    Update input.dart

--- input.dart
+++ input.dart
@@ -14,27 +14,23 @@ dynamic nettGst;
 dynamic nettDisc;
 
 class Code {
-  //properties
   var _num;
 
-  //constructor
   Code() {
     _num = " ";
   }
 
-  //setter
   void setCode(dynamic code) {
     this._num = code;
   }
 
-  //getter
   String getNum() {
     return _num;
   }
 
+//Zahra Fathanah (2019050)
   void menu() {
     print("||--------------Math'am Al-Mufid--------------||");
-//Zahra Fathanah (2019050)
     Map<int, String> foods = {
       1: 'Bukhari Ayam    | RM 30.25',
       2: 'Bukhari Kambing | RM 40.15',
@@ -49,80 +45,14 @@ class Code {
     foods.forEach((code, food) => print('$code. ${food}'));
   }
 
-//Nur Khadijah (2015546) and //Zahra Fathanah (2019050)
+//Nur Khadijah (2015546)
   void input() {
-    stdout.write('Enter Product Code: ');
-    code = int.parse(stdin.readLineSync()!);
-
-    if (code == 1) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 30.25;
-      print('Bukhari Ayam [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 2) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 40.15;
-      print('Bukhari Kambing [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 3) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 30.25;
-      print('Briyani Ayam [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 4) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 40.15;
-      print('Briyani Kambing [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 5) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 12.25;
-      print('Tojin Lahm [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 6) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 7.65;
-      print('Shakshuka [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 7) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 7.50;
-      print('Onion Rings [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 8) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 8.00;
-      print('Shadr Maqlyah [RM $price] X $quantity');
-      subTotal();
-      add();
-    } else if (code == 9) {
-      stdout.write('Enter Quantity: ');
-      quantity = int.parse(stdin.readLineSync()!);
-      price = 5.35;
-      print('Mahhalaby [RM $price] X $quantity');
-      subTotal();
-      add();
-    }
+    stdout.write('Enter Quantity: ');
+    quantity = int.parse(stdin.readLineSync()!);
   }
 
 //Zahra Fathanah (2019050)
   void subTotal() {
-    print('<<--------------------Recipt-------------------->>');
     NoI += quantity;
     print('Quantity: $quantity');
     print('Number Of Item = $NoI');
@@ -130,14 +60,15 @@ class Code {
     print('SubTotal = RM $subT');
   }
 
-//Nur Khadijah (2015546)
   void count() {
-    stdout.write('Discount(%): ');
+//Zahra Fathanah (2019050)
+    print('<<--------------------Recipt-------------------->>');
+    stdout.write('Discount(%) = ');
     double? disc = double.parse(stdin.readLineSync()!);
     discount = (disc / 100 * subT);
     nettDisc = discount.toStringAsFixed(2);
-    print('(-)RM $discount');
 
+//Nur Khadijah (2015546)
     gst = (0.06 * subT);
     nettGst = gst.toStringAsFixed(2);
     print('GST 6% = $nettGst');
@@ -146,7 +77,4 @@ class Code {
     nettTotal = total.toStringAsFixed(2);
     print('Total = RM $nettTotal');
   }
-
-//Nur Khadijah (2015546)
-  void add() {}
 }
